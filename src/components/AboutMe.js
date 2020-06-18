31eb253c3a33dc598c087d7f9e0ec9a93b732570
thomasvn <thomasnguyen96@gmail.com>
2020-06-18 11:17:20 -0700

    Beginning work on site banner

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,8 +5,10 @@ class AboutMe extends React.Component {
   render() {
     return (
       <div className="AboutMe">
-        <h1 className="title">Thomas Nguyen</h1>
-        <img src={portrait} className="portrait" alt="Thomas's Portrait"></img>
+        <div className="banner">
+          <h1 className="title">Thomas Nguyen</h1>
+          <img src={portrait} className="portrait" alt="Thomas's Portrait"></img>
+        </div>
         <p>Hi! Glad to have you here.</p>
         <p>This site's still under construction so please do come back soon.</p>
       </div>
